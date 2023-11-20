09f636d2e4645d9973c930c9664734be05eb7577
saachit1208 <32326876+saachit1208@users.noreply.github.com>
2023-11-20 12:31:55 -0600

    Actors History SCD
    actors_history_scd backfill

---
+++ all_tracks/dimensional_modeling/submission/query_4.sql
@@ -0,0 +1,63 @@
+INSERT INTO stalwai.actors_history_scd
+WITH
+  lagged AS (
+    SELECT
+      actor,
+      actorid,
+      quality_class,
+      CASE
+        WHEN is_active THEN 1
+        ELSE 0
+      END AS is_active,
+      LAG(quality_class, 1) OVER (
+          PARTITION BY
+            actor, actorid
+          ORDER BY
+            current_year
+        ) AS quality_class_last_year, 
+      CASE
+        WHEN LAG(is_active, 1) OVER (
+          PARTITION BY
+            actor, actorid
+          ORDER BY
+            current_year
+        ) THEN 1
+        ELSE 0
+      END AS is_active_last_year,
+      current_year
+    FROM
+      stalwai.actors
+    WHERE
+      current_year <= 1931
+  ),
+  streaked AS (
+    SELECT
+      *,
+      SUM(
+        CASE
+          WHEN is_active <> is_active_last_year OR quality_class <> quality_class_last_year THEN 1
+          ELSE 0
+        END
+      ) OVER (
+        PARTITION BY
+          actor, actorid
+        ORDER BY
+          current_year
+      ) AS streak_identifier
+    FROM
+      lagged
+  )
+SELECT
+  actor,
+  actorid,
+  MAX(quality_class) as quality_class,
+  MAX(is_active) = 1 AS is_active,
+  MIN(current_year) AS start_year,
+  MAX(current_year) AS end_year,
+  1931 AS current_year
+FROM
+  streaked
+GROUP BY
+  actor,
+  actorid,
+  streak_identifier
